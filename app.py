3f30ebd6c6521036c6cbf15861ad2a76f6375adf
junholorran <70176954+junholorran@users.noreply.github.com>
2026-09-26 15:26:24 +0100

    Allow one-shot PDH PDL replay on experimental startup

--- app.py
+++ app.py
@@ -1904,6 +1904,8 @@ def _kairos_btc_live_scanner_loop():
 
 if os.environ.get('RAILWAY_SERVICE_NAME') == 'kairos-poi-abc-sol':
     threading.Thread(target=_kairos_btc_live_scanner_loop, daemon=True).start()
+    if os.environ.get('KAIROS_RUN_PDH_PDL_1D_ONCE') == '1':
+        threading.Thread(target=_run_kairos_pdh_pdl_btc_background,args=(1,None),daemon=True).start()
     print('[KAIROS_BTC_LIVE] scanner ENABLED BTCUSD A_CURRENT interval=5m demo/manual', flush=True)
 
 
